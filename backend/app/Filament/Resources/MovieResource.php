8030428fdd73717584d9dd417b04d35a20c47534
Darius M. <misevicius.darius@gmail.com>
2025-11-11 15:29:42 +0200

    filament integration

---
+++ backend/app/Filament/Resources/MovieResource.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MovieResource\Pages;
+use App\Filament\Resources\MovieResource\RelationManagers;
+use App\Models\Movie;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MovieResource extends Resource
+{
+    protected static ?string $model = Movie::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\Textarea::make('description')
+                    ->required()
+                    ->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                //
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMovies::route('/'),
+            'create' => Pages\CreateMovie::route('/create'),
+            'edit' => Pages\EditMovie::route('/{record}/edit'),
+        ];
+    }
+}
